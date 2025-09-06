a4b8d6048c2c09a355140632a2a4883b2bc21eda
Shih-Shinh Huang <powwhuang@gmail.com>
2025-09-06 10:15:57 +0800

    Add files via upload

---
+++ unit_list.js
@@ -0,0 +1,21 @@
+// unit_data.js
+const unit_data = [
+  {
+    title: "Histogram Equalization",
+    description: "An image enhancement algorithm that makes the histogram of the resulting image closer to a uniform distribution",
+    link: "./classic_DIP/histogram_equalization.html"
+  },
+    
+  {
+    title: "Histogram Matching",
+    description: "An image enhancement algorithm that adjusts the intensity distribution of one image so that it resembles the histogram of a reference image. ",
+    link: "./classic_DIP/histogram_matching.html"
+  },
+    
+  {
+    title: "Otsu Thresholding:",
+    description: "A global image thresholding method used in image processing to automatically separate an image into two classes. ",
+    link: "./classic_DIP/otsu_thresholding.html"
+  }
+];
+
