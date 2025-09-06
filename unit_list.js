24f0b3fee12d3e77a41cade53aafa0119850084e
Shih-Shinh Huang <powwhuang@gmail.com>
2025-09-06 14:53:55 +0800

    Add files via upload

--- unit_list.js
+++ unit_list.js
@@ -11,11 +11,24 @@ const unit_data = [
     description: "An image enhancement algorithm that adjusts the intensity distribution of one image so that it resembles the histogram of a reference image. ",
     link: "./classic_DIP/histogram_matching.html"
   },
-    
+
+  {
+    title: "Hough Transform",
+    description: "A technique to detect geometric shapes (like lines, circles, or ellipses) in an image by parameterization and voting. ",
+    link: "./classic_DIP/Hough_transform.html"
+  },
+
   {
     title: "Otsu Thresholding:",
     description: "A global image thresholding method used in image processing to automatically separate an image into two classes. ",
     link: "./classic_DIP/otsu_thresholding.html"
+  },
+
+  {
+    title: "Tw-Pass Connected Component Labeling:",
+    description: "A classic algorithm to detect and label distinct connected regions (components) in a binary image. ",
+    link: "./classic_DIP/two_pass_connected_components.html"
   }
+
 ];
 
